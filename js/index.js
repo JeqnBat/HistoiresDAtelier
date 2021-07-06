7433ccd6510e93ce80f47bd58c97ea601dd89800
JeqnBat <jbpellier@noos.fr>
2021-07-06 16:26:07 +0200

    Homepage responsive layout script scenario

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
 // IMPORTATOR _____________________________________________ */
 import Wrapper from './mobile/components/wrapper.js'
+import { deviceIsMobile } from './scripts.js'
 
 // INSTANTIATE ELEMENTS INSIDE THE PARENT DOM _____________ */
-const wrapper = document.createElement('wrapper-m')
-const main = document.querySelector('main')
+function init() {
+    const main = document.querySelector('main')
+    if (deviceIsMobile()) {
+        const wrapper = document.createElement('wrapper-m')
+        const main = document.querySelector('main')
 
-wrapper.setAttribute('name', 'wrapper')
-main.appendChild(wrapper)
+        wrapper.setAttribute('name', 'wrapper')
+        main.appendChild(wrapper)
+    } else {
+        const msg = document.createElement('p')
+        msg.innerText = 'NOT A MOBILE VIEWPORT'
+        main.appendChild(msg)
+    }
+}
 
-const footer = document.createElement('footer-m')
-document.querySelector('footer').appendChild(footer)
+// CALL INIT ______________________________________________ */
+init()
+
+
