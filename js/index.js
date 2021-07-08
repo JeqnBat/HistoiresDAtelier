83fa3d81c8568c21afc9a13f682a82761eb2e913
JeqnBat <jbpellier@noos.fr>
2021-07-08 19:51:18 +0200

    Code cleansing: delete useless files & properties

--- js/index.js
+++ js/index.js
@@ -1,8 +1,32 @@
 // IMPORTATOR _____________________________________________ */
 import Wrapper from './mobile/components/wrapper.js'
-import { deviceIsMobile } from './scripts.js'
+
+// DETECT IF DEVICE IS MOBILE _____________________________ */
+/**
+ * <b>DESCR:</b><br>
+ * Checks if device has a touchscreen AND viewport width smaller
+ * than 500 pixels
+ * 
+ * @function
+ * @returns {boolean} TRUE if the 2 parameters are valid
+ */
+ function deviceIsMobile() {
+    let deviceParameters = {
+        touchScreen: !!navigator.maxTouchPoints,
+        width: window.innerWidth < 500 ? true : false
+    }
+    return Object.values(deviceParameters).every(Boolean)
+}
 
 // INSTANTIATE ELEMENTS INSIDE THE PARENT DOM _____________ */
+/**
+ * <b>DESCR:</b><br>
+ * First function to be called once page is loaded.
+ * Detects type of viewport (mobile / desktop) & prints
+ * the web components accordingly.
+ * 
+ * @function
+ */
 function init() {
     const main = document.querySelector('main')
     if (deviceIsMobile()) {
@@ -14,6 +38,7 @@ function init() {
     } else {
         const msg = document.createElement('p')
         msg.innerText = 'NOT A MOBILE VIEWPORT'
+
         main.appendChild(msg)
     }
 }
